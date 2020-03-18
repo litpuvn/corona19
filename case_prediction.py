21e13dad41a798766ac0a5c651820df1e8a66e05
Hoang Long <litpuvn@gmail.com>
2020-03-18 15:06:36 -0500

    add case prediction

---
+++ case_prediction.py
@@ -0,0 +1,129 @@
+import datetime
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from keras.layers.recurrent import LSTM
+from pandas.core.frame import DataFrame
+from pandas import concat
+from common.utils import split_train_validation_test, flatten_test_predict
+
+filepath = 'data/gtrends.csv'
+df_gtrends = pd.read_csv(filepath, sep=',', header=0, index_col=0, parse_dates=True)
+df_gtrends = df_gtrends[37:]
+
+df_new_cases = pd.read_csv('data/new_cases.csv', sep=',', header=0, index_col=0, parse_dates=True)
+df_new_cases = df_new_cases[:-1]
+df_new_cases.fillna(value=0, inplace=True)
+
+# countries = ['Italy', 'Spain', 'France', 'Germany']
+countries = ['United Kingdom', 'South Korea', 'United States', 'China']
+seltected_index = 2
+selected_country = countries[seltected_index]
+
+selected_country_cases_df = DataFrame(df_new_cases[selected_country].values, index=df_new_cases.index)
+selected_country_gtrends_df = DataFrame(df_gtrends[selected_country].values, index=df_gtrends.index)
+
+concatenated_dfs = [
+    selected_country_cases_df,
+    selected_country_gtrends_df
+]
+
+
+combined_data = concat(concatenated_dfs, axis=1)
+
+HORIZON = 1
+time_step_lag = 1
+
+multi_time_series = DataFrame(combined_data)
+multi_time_series.columns = ['load', 'trends']
+features = ["load", "trends"]
+
+valid_start_dt = '2020-02-29'
+test_start_dt = '2020-03-07'
+
+train_inputs, valid_inputs, test_inputs, y_scaler = split_train_validation_test(multi_time_series,
+                                                                                valid_start_time=valid_start_dt,
+                                                                                test_start_time=test_start_dt,
+                                                                                time_step_lag=time_step_lag,
+                                                                                horizon=HORIZON,
+                                                                                features=features,
+                                                                                target=["load"],
+                                                                                time_format='%Y-%m-%d',
+                                                                                freq='d'
+                                                                                )
+
+print('done')
+
+## build CNN
+from keras.models import Model, Sequential
+from keras.layers import Conv1D, Dense, Flatten
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from sklearn.metrics import explained_variance_score
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_log_error
+from sklearn.metrics import median_absolute_error
+from sklearn.metrics import r2_score
+from math import sqrt
+
+from common.utils import split_train_validation_test, mape
+
+LATENT_DIM = 16
+BATCH_SIZE = 32
+EPOCHS = 50
+
+X_train = train_inputs['X']
+y_train = train_inputs['target_load']
+
+X_valid = valid_inputs['X']
+y_valid = valid_inputs['target_load']
+
+# input_x = train_inputs['X']
+print("train_X shape", X_train.shape)
+print("valid_X shape", X_valid.shape)
+# print("target shape", y_train.shape)
+# print("training size:", len(train_inputs['X']), 'validation', len(valid_inputs['X']), 'test size:', len(test_inputs['X']) )
+# print("sum sizes", len(train_inputs['X']) + len(valid_inputs['X']) + len(test_inputs['X']))
+model = Sequential()
+model.add(LSTM(LATENT_DIM, input_shape=(time_step_lag, len(features))))
+model.add(Dense(1))
+model.compile(optimizer='RMSprop', loss='mse')
+model.summary()
+
+earlystop = EarlyStopping(monitor='val_loss', patience=10)
+history = model.fit(X_train,
+                    y_train,
+                    batch_size=BATCH_SIZE,
+                    epochs=EPOCHS,
+                    validation_data=(X_valid, y_valid),
+                    callbacks=[earlystop],
+                    verbose=1)
+
+# Test the model
+X_test = test_inputs['X']
+y1_test = test_inputs['target_load']
+
+y1_preds = model.predict(X_test)
+
+if y_scaler is not None:
+    y1_test = y_scaler.inverse_transform(y1_test)
+    y1_preds = y_scaler.inverse_transform(y1_preds)
+
+y1_test, y1_preds = flatten_test_predict(y1_test, y1_preds)
+mse = mean_squared_error(y1_test, y1_preds)
+
+rmse_predict = sqrt(mse)
+evs = explained_variance_score(y1_test, y1_preds)
+mae = mean_absolute_error(y1_test, y1_preds)
+mse = mean_squared_error(y1_test, y1_preds)
+
+meae = median_absolute_error(y1_test, y1_preds)
+r_square = r2_score(y1_test, y1_preds)
+# mape_v = mape(y1_preds.reshape(-1, 1), y1_test.reshape(-1, 1))
+
+# print("mse:", mse, 'rmse_predict:', rmse_predict, "mae:", mae, "mape:", mape_v, "r2:", r_square,
+#       "meae:", meae, "evs:", evs)
+
+
+print('rmse_predict:', rmse_predict, "evs:", evs, "mae:", mae,
+      "mse:", mse, "meae:", meae, "r2:", r_square)
---
+++ common/utils.py
@@ -0,0 +1,124 @@
+import numpy as np
+import pandas as pd
+import os
+from collections import UserDict
+
+from pandas.core.indexes.datetimes import DatetimeIndex
+import datetime as dt
+
+from sklearn.preprocessing.data import MinMaxScaler
+from sklearn.preprocessing.data import StandardScaler
+
+from common.TimeseriesTensor import TimeSeriesTensor
+from sklearn.utils import check_array
+
+
+
+
+def split_train_validation_test(multi_time_series_df, valid_start_time, test_start_time, features,
+                                time_step_lag=1, horizon=1, target='target', time_format='%Y-%m-%d %H:%M:%S', freq='H'):
+
+    if not isinstance(features, list) or len(features) < 1:
+        raise Exception("Bad input for features. It must be an array of dataframe colummns used")
+
+    train = multi_time_series_df.copy()[multi_time_series_df.index < valid_start_time]
+    train_features = train[features]
+    train_targets = train[target]
+
+    # X_scaler = MinMaxScaler()
+    # target_scaler = MinMaxScaler()
+    # y_scaler = MinMaxScaler()
+
+    X_scaler = None
+    target_scaler = None
+    y_scaler = None
+
+    # X_scaler = StandardScaler()
+    # target_scaler = StandardScaler()
+    # y_scaler = StandardScaler()
+
+
+    # 'load' is our key target. If it is in features, then we scale it.
+    # if it not 'load', then we scale the first column
+    if 'load' in features:
+        tg = train[['load']]
+        if y_scaler is not None:
+            y_scaler.fit(tg)
+    else:
+
+        tg = train[target]
+        ## scale the first column
+        if y_scaler is not None:
+            y_scaler.fit(tg.values.reshape(-1, 1))
+
+    if target_scaler is not None:
+        train[target] = target_scaler.fit_transform(train_targets)
+
+    if X_scaler is not None:
+        X_scaler.fit(train_features)
+        train[features] = X_scaler.transform(train_features)
+
+    tensor_structure = {'X': (range(-time_step_lag + 1, 1), features)}
+    train_inputs = TimeSeriesTensor(train, target=target, H=horizon, freq=freq, tensor_structure=tensor_structure)
+
+    print(train_inputs.dataframe.head())
+
+
+    look_back_dt = dt.datetime.strptime(valid_start_time, time_format) - dt.timedelta(hours=time_step_lag - 1)
+    valid = multi_time_series_df.copy()[(multi_time_series_df.index >= look_back_dt) & (multi_time_series_df.index < test_start_time)]
+    valid_features = valid[features]
+
+    if X_scaler is not None:
+        valid[features] = X_scaler.transform(valid_features)
+    tensor_structure = {'X': (range(-time_step_lag + 1, 1), features)}
+    valid_inputs = TimeSeriesTensor(valid, target=target, H=horizon, freq=freq, tensor_structure=tensor_structure)
+
+    print(valid_inputs.dataframe.head())
+
+    # test set
+    # look_back_dt = dt.datetime.strptime(test_start_time, '%Y-%m-%d %H:%M:%S') - dt.timedelta(hours=time_step_lag - 1)
+    test = multi_time_series_df.copy()[test_start_time:]
+    test_features = test[features]
+
+    if X_scaler is not None:
+        test[features] = X_scaler.transform(test_features)
+    test_inputs = TimeSeriesTensor(test, target=target, H=horizon, freq=freq, tensor_structure=tensor_structure)
+
+    print("time lag:", time_step_lag, "original_feature:", len(features))
+
+    return train_inputs, valid_inputs, test_inputs, y_scaler
+
+
+def mape(predictions, actuals):
+    predictions = check_array(predictions)
+    actuals = check_array(actuals)
+
+    """Mean absolute percentage error"""
+    return ( np.mean(np.abs(predictions - actuals) / actuals))
+
+
+def create_evaluation_df(predictions, test_inputs, H, scaler):
+    """Create a data frame for easy evaluation"""
+    eval_df = pd.DataFrame(predictions, columns=['t+'+str(t) for t in range(1, H+1)])
+    eval_df['timestamp'] = test_inputs.dataframe.index
+    eval_df = pd.melt(eval_df, id_vars='timestamp', value_name='prediction', var_name='h')
+    eval_df['actual'] = np.transpose(test_inputs['target']).ravel()
+    eval_df[['prediction', 'actual']] = scaler.inverse_transform(eval_df[['prediction', 'actual']])
+    return eval_df
+
+
+
+def flatten_test_predict(y_tests, y_predicts):
+    if len(y_tests) == 1:
+        y_tests = y_tests[0]
+
+    if len(y_predicts) == 1:
+        y_predicts = y_predicts[0]
+
+    if not isinstance(y_tests, np.ndarray) or not isinstance(y_predicts, np.ndarray):
+        raise Exception('bad input data for y_tests or y_predicts')
+
+    y_tests = y_tests.ravel()
+    y_predicts = y_predicts.ravel()
+
+    return y_tests, y_predicts
