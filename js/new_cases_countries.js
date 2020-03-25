b693166be7ffc7ba79335d6e4bc02b1fd0059fa1
Hoang Long <litpuvn@gmail.com>
2020-03-25 15:51:48 -0500

    add new cases

--- js/new_cases_countries.js
+++ js/new_cases_countries.js
@@ -2,7 +2,7 @@ var chart = c3.generate({
     bindto: '#chart',
     data: {
         x: 'x',
-       xFormat: '%m/%d/%Y', // 'xFormat' can be used as custom format of 'x'
+       xFormat: '%Y-%m-%d', // 'xFormat' can be used as custom format of 'x'
         columns: []
 
     },
@@ -22,51 +22,34 @@ d3.csv("data/new_cases_march_25.csv")
     .then(function(datarows){
 
         let chart_data = [];
-           let end_date = new Date(2020, 3, 23);
-            let cases_count;
-            let timeseries_data = ['x'];
-            let formatTimeX = d3.timeFormat("%m/%d/%Y");
-            let date_string;
-            for (let d = new Date(2020, 1, 22); d <= end_date; d.setDate(d.getDate() + 1)) {
-                date_string = formatTimeX(d);
-                timeseries_data.push(date_string);
-            }
-
-            chart_data.push(timeseries_data);
-
-            let countries = {};
-            let formatTime = d3.timeFormat("%-m/%-d/%Y");
-
-        datarows.forEach(function(row, index){
-            let country = row['Country/Region'];
-            // if (country != 'China') {
-            //     return;
+           // let end_date = new Date(2020, 2, 23);
+            let timeseries_data_x = ['x'];
+            // let formatTimeX = d3.timeFormat("%m/%d/%Y");
+            // let date_string;
+            // for (let d = new Date(2020, 0, 22); d <= end_date; d.setDate(d.getDate() + 1)) {
+            //     date_string = formatTimeX(d);
+            //     timeseries_data.push(date_string);
             // }
-            if (countries.hasOwnProperty(country) == true) {
+            //
+            // chart_data.push(timeseries_data);
+
+            let formatTime = d3.timeFormat("%-m/%-d/%Y");
+            let timeseries_data = ['world'];
+            let worldCases;
+        datarows.forEach(function(row, index){
+
+            if (index < 22){ // not working with date before Jan 22, 2020
                 return;
             }
-            countries[country] = true;
-            // month has index 0
-            let end_date = new Date(2020, 2, 23);
-            let cases_count;
-            let timeseries_data = [country];
-            let timestring = formatTime(end_date);
-            // let sum_cases = 0;
-            for (let d = new Date(2020, 0, 22); d <= end_date; d.setDate(d.getDate() + 1)) {
-                timestring = formatTime(d);
-                cases_count = +row[timestring] || 0;
-                timeseries_data.push(cases_count);
-            }
 
-            // console.log(country + ";sum=" + sum_cases);
-            // if (sum_cases > 3000){
-            //     chart_data.push(timeseries_data);
-            // }
-
-
+            worldCases = +row['World'] || 0;
+            timeseries_data.push(worldCases);
+            timeseries_data_x.push(row['date'])
 
         });
 
+        chart_data.push(timeseries_data_x);
+        chart_data.push(timeseries_data);
         chart.load({
             columns: chart_data
         });
