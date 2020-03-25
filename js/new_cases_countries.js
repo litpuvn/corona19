76f884b7792f80d0e20f6c85d541f6587345eecc
Hoang Long <litpuvn@gmail.com>
2020-03-25 15:40:32 -0500

    add confirmed cases

--- js/new_cases_countries.js
+++ js/new_cases_countries.js
@@ -2,7 +2,7 @@ var chart = c3.generate({
     bindto: '#chart',
     data: {
         x: 'x',
-        xFormat: '%m/%d/%Y', // 'xFormat' can be used as custom format of 'x'
+       xFormat: '%m/%d/%Y', // 'xFormat' can be used as custom format of 'x'
         columns: []
 
     },
@@ -18,16 +18,16 @@ var chart = c3.generate({
 
 
 
-d3.csv("data/time_series_covid19_confirmed_global_complete.csv")
+d3.csv("data/new_cases_march_25.csv")
     .then(function(datarows){
 
         let chart_data = [];
-           let end_date = new Date(2020, 2, 23);
+           let end_date = new Date(2020, 3, 23);
             let cases_count;
             let timeseries_data = ['x'];
             let formatTimeX = d3.timeFormat("%m/%d/%Y");
             let date_string;
-            for (let d = new Date(2020, 0, 22); d <= end_date; d.setDate(d.getDate() + 1)) {
+            for (let d = new Date(2020, 1, 22); d <= end_date; d.setDate(d.getDate() + 1)) {
                 date_string = formatTimeX(d);
                 timeseries_data.push(date_string);
             }
@@ -37,10 +37,6 @@ d3.csv("data/time_series_covid19_confirmed_global_complete.csv")
             let countries = {};
             let formatTime = d3.timeFormat("%-m/%-d/%Y");
 
-            // let colorArray = [d3.scale.category20()];
-            let colores_g = ["#3366cc", "#dc3912", "#ff9900", "#109618", "#990099", "#0099c6", "#dd4477", "#66aa00", "#b82e2e", "#316395", "#994499", "#22aa99", "#aaaa11", "#6633cc", "#e67300", "#8b0707", "#651067", "#329262", "#5574a6", "#3b3eac"];
-            let colorIndex = 0;
-            let colors = {};
         datarows.forEach(function(row, index){
             let country = row['Country/Region'];
             // if (country != 'China') {
@@ -63,19 +59,16 @@ d3.csv("data/time_series_covid19_confirmed_global_complete.csv")
             }
 
             // console.log(country + ";sum=" + sum_cases);
-            if (cases_count > 5000){
-                chart_data.push(timeseries_data);
-                colors[country] = colores_g[colorIndex];
-                colorIndex = colorIndex + 1
-            }
+            // if (sum_cases > 3000){
+            //     chart_data.push(timeseries_data);
+            // }
 
 
 
         });
 
         chart.load({
-            columns: chart_data,
-            colors: colors
+            columns: chart_data
         });
 
     })
