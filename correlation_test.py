2468a5dc9dc7da77514eb34e7604b1e8ee3129ee
Hoang Long <litpuvn@gmail.com>
2020-04-01 14:22:02 -0500

    add correlation test

--- correlation_test.py
+++ correlation_test.py
@@ -12,14 +12,15 @@ from pandas import concat
 from common.utils import split_train_validation_test, flatten_test_predict, store_predict_points
 from keras.models import Model as KerasModel
 from scipy.stats import pearsonr
+import os
 
-filepath = 'data/gtrends.csv'
+filepath = 'data/gtrends_coronavirus.csv'
 df_gtrends = pd.read_csv(filepath, sep=',', header=0, index_col=0, parse_dates=True)
 df_gtrends = df_gtrends[37:]
 df_gtrends['Worldwide'] = df_gtrends.sum(axis=1)
 
-df_new_cases = pd.read_csv('data/new_cases.csv', sep=',', header=0, index_col=0, parse_dates=True)
-df_new_cases = df_new_cases[:-1]
+df_new_cases = pd.read_csv('data/new_cases_april_01.csv', sep=',', header=0, index_col=0, parse_dates=True)
+df_new_cases = df_new_cases[21:-12]
 df_new_cases.fillna(value=0, inplace=True)
 
 # countries = ['Italy', 'Spain', 'France', 'Germany']
@@ -27,12 +28,34 @@ df_new_cases.fillna(value=0, inplace=True)
 # selected_index = 4
 # selected_country = countries[selected_index]
 #
-# selected_country_cases_df = DataFrame(df_new_cases[selected_country].values, index=df_new_cases.index)
-# selected_country_gtrends_df = DataFrame(df_gtrends[selected_country].values, index=df_gtrends.index)
 
-countries = df_gtrends.columns
+folder = 'data/daily-complete'
+
+def get_gtrend_data_by_keyword(keyword):
+    global folder
+    file_folder = os.path.join(folder, keyword.replace(' ', '-', 5))
+    gtrends_interest = pd.read_csv(os.path.join(file_folder, '2020-01-20-2020-03-23-interest.csv'), sep=',', header=0, index_col=0, parse_dates=True)
+    # gtrends_related_queries = pd.read_csv(os.path.join(file_folder, '2020-01-20-2020-03-23-top-related-queries.csv'), sep=',', header=0, index_col=0, parse_dates=True)
+    # gtrends_related_topics = pd.read_csv(os.path.join(file_folder, '2020-01-20-2020-03-23-top-related-topic.csv'), sep=',', header=0, index_col=0, parse_dates=True)
+
+    # return gtrends_interest, gtrends_related_queries, gtrends_related_topics
+    return gtrends_interest
+
+
+countries = ['World']
+keywords = ['cases of covid19', 'corona', 'coronavirus', 'coronavirus cases', 'coronavirus covid19',
+            'coronavirus news', 'coronavirus symptoms', 'coronavirus update', 'covid', 'covid 19',
+            'covid 19 cases', 'covid19', 'covid19 cases'
+            ]
+
 for country in countries:
     if country not in df_new_cases.columns:
         continue
-    corr, pval = pearsonr(df_new_cases[country].values, df_gtrends[country].values)
-    print('correlation: new case vs gtrends', country,"; corr:", corr, "p-val:", pval)
+    country_new_cases_df = df_new_cases[country]
+    for kw in keywords:
+        gtrends_df = get_gtrend_data_by_keyword(kw)
+        remove_rows = 60-len(gtrends_df)
+        if remove_rows < 0:
+            gtrends_df = gtrends_df[:remove_rows]
+        corr, pval = pearsonr(country_new_cases_df.values, gtrends_df[kw].values)
+        print('correlation:', kw, ':new case vs gtrends:', country,"; corr:", corr, "p-val:", pval)
