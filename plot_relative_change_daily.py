c50b89291a9f7761771c71f70165441a68721314
Hashim Abu-gellban <gellban@gmail.com>
2020-04-16 22:54:12 -0500

    improve the figures generated by plot_relative_change_daily.py

--- plot_relative_change_daily.py
+++ plot_relative_change_daily.py
@@ -8,26 +8,8 @@ import os
 from datetime import timedelta, date
 import pandas as pd
 
-np.random.seed(19680801)
-plt.rcdefaults()
-
-fig, ax = plt.subplots(figsize=(1, 9))
-
-start_year = 2020
-start_mon = 1
-start_day = 21
-
-start_date = date(start_year, start_mon, start_day)
-day_count = 61
-
-days = []
-colors = []
-
-keyword = 'covid19 cases'
-folder = os.path.join('data/daily-complete', keyword.replace(' ', '-', 5))
-
-def extract_data(filepath):
-    global keyword
+def extract_data(filepath, keyword):
+    # global keyword
 
     try:
         df = pd.read_csv(filepath, sep=',', header=0, index_col=0, parse_dates=True)
@@ -36,7 +18,7 @@ def extract_data(filepath):
 
         val1 = df[keyword].values[0]
         val2 = df[keyword].values[1]
-        print("val1:", val1, ";val2:", val2, ";change:", (val2-val1))
+        # print("val1:", val1, ";val2:", val2, ";change:", (val2-val1))
         return (val2 - val1)
     except:
         print('Error reading file:', filepath)
@@ -45,6 +27,7 @@ def extract_data(filepath):
 
 
 def get_color(index, change):
+    i = index
     if i % 2 == 0:
         if change > 0:
             return '#344e9d'
@@ -56,36 +39,90 @@ def get_color(index, change):
     return '#fc987d'
 
 
-performance = []
-for i in range(day_count):
+def plot_graph(keyword, is_last=False):
+    np.random.seed(19680801)
+    plt.rcdefaults()
 
-    day_string = start_date.strftime('%Y-%m-%d')
-    filename = os.path.join(folder, day_string + '-interest.csv')
+    fig, ax = plt.subplots(figsize=(1, 9))
 
-    if not os.path.isfile(filename):
-        print('File', filename, 'does not exist')
-        continue
+    start_year = 2020
+    start_mon = 1
+    start_day = 21
 
-    print('processing for file: ', filename)
+    start_date = date(start_year, start_mon, start_day)
+    day_count = 61
 
-    days = days + [day_string]
-    start_date = start_date + timedelta(1)
+    days = []
+    colors = []
 
-    val = extract_data(filepath=filename)
-    performance = performance + [val]
+    folder = os.path.join('data/daily-complete', keyword.replace(' ', '-', 5))
 
-    my_color = get_color(index=i, change=val)
-    colors = colors + [my_color]
-# Example data
+    performance = []
+    for i in range(day_count):
 
-y_pos = np.arange(len(days))
-ax.barh(y_pos, performance, color=colors, xerr=None, align='center')
-# ax.set_yticks(y_pos)
-ax.set_yticklabels([])
-ax.invert_yaxis()  # labels read top-to-bottom
-# ax.set_xlabel('Relative Change (%)')
-ax.set_title(keyword)
+        day_string = start_date.strftime('%Y-%m-%d')
+        filename = os.path.join(folder, day_string + '-interest.csv')
+        # filename = os.path.abspath(filename)
+        # print('os.path.abspath(filename)', os.path.abspath(filename))
+        start_date = start_date + timedelta(1)
+        if not os.path.isfile(filename):
+            print('File', filename, 'does not exist')
+            continue
 
-# plt.ylim(-2, 2)
+        # print('processing for file: ', filename)
 
-plt.show()
+        days = days + [day_string]
+        # start_date = start_date + timedelta(1)
+
+        val = extract_data(filename, keyword)
+        performance = performance + [val]
+
+        my_color = get_color(index=i, change=val)
+        colors = colors + [my_color]
+    # Example data
+
+    y_pos = np.arange(len(days))
+    # print('days length:', len(days),', values:', days)
+    ax.barh(y_pos, performance, color=colors, xerr=None, align='center')
+    # ax.set_yticks(y_pos)
+    ax.set_yticklabels([])
+    #4/15
+    if is_last:
+        ax.set_yticklabels(days)
+    ax.set_xticklabels([])
+
+    ax.invert_yaxis()  # labels read top-to-bottom
+
+    # ax.set_xlabel('Relative Change (%)')
+    # ax.set_title(keyword)
+
+    plt.xlim(-100, 100)
+    # plt.xticks(np.arange(-25, 100, step=100))
+
+    # plt.show()
+    out_path = 'figures/'
+    out_filename = out_path + 'daily-change-of-' + keyword + '.png'
+    out_filename = out_filename.replace(" ", "-")
+    print('The figure save into:', out_filename)
+    plt.savefig(out_filename, dpi=200, bbox_inches='tight') #save figure as ward_clusters
+    # plt.show()
+    # plt.close()
+
+
+#################################################
+#############  MAIN  ############################
+#################################################
+keywords = ['cases of covid19', 'corona', 'coronavirus', 'coronavirus cases', 'coronavirus covid19',
+            'coronavirus news', 'coronavirus symptoms', 'coronavirus update', 'covid', 'covid 19',
+            'covid 19 cases', 'covid19', 'covid19 cases'
+            ]
+
+# keyword = 'covid19 cases'
+for i in range(len(keywords)):
+    keyword = keywords[i]
+    # keyword = 'cases of covid19'
+    # keyword = 'covid19 cases'
+    is_last = i==(len(keywords)-1)
+    # is_last = False
+    plot_graph(keyword, is_last=is_last)
+    # break
